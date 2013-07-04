3ee1e1d6692b8faf85edfa0b5e3d6cb26a0b0721
Jan Potoms <potoms.jan@gmail.com>
2013-07-04 21:00:47 +0200

    Use id to restore previous state

--- src/background.js
+++ src/background.js
@@ -7,10 +7,8 @@
     
     chrome.app.runtime.onLaunched.addListener(function () {
         chrome.app.window.create('index.html', {
-            'bounds': {
-                'width': 400,
-                'height': 500
-            }
+            'id': 'brackets'
         });
-    });
+    });  
+    
 }());
