d71ec9f905724649ae7f03b0ad6e9bb398b79f76
Jan Potoms <potoms.jan@gmail.com>
2013-07-04 20:31:53 +0200

    Make jslint happy

--- src/background.js
+++ src/background.js
@@ -1,8 +1,16 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
-});
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global chrome */
+
+(function () {
+    "use strict";
+    
+    chrome.app.runtime.onLaunched.addListener(function () {
+        chrome.app.window.create('index.html', {
+            'bounds': {
+                'width': 400,
+                'height': 500
+            }
+        });
+    });
+}());
